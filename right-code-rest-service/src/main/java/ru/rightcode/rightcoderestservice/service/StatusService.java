ebb608f8f519b0f05e0b94d7fccc80d6a3501cda
MikhailBatukhtin <giezz@vk.com>
2023-07-06 21:48:36 +0300

    Branch 'dev-misha' started.

---
+++ right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/service/StatusService.java
@@ -0,0 +1,23 @@
+package ru.rightcode.rightcoderestservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.rightcode.rightcoderestservice.model.Status;
+import ru.rightcode.rightcoderestservice.repository.StatusRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StatusService {
+
+    private final StatusRepository statusRepository;
+
+    public List<Status> getAllStatuses() {
+        return statusRepository.findAll();
+    }
+
+    public void addStatus(Status status) {
+        statusRepository.save(status);
+    }
+}
