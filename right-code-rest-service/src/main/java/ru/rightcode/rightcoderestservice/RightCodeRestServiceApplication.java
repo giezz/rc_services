fd2b390d517a83a50b7651c83afced30d4bf89c8
MikhailBatukhtin <giezz@vk.com>
2023-07-08 01:52:42 +0300

    Добавление @EntityGraph в ArticleRepository для решение проблемы N+1.

--- right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/RightCodeRestServiceApplication.java
+++ right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/RightCodeRestServiceApplication.java
@@ -2,7 +2,6 @@ package ru.rightcode.rightcoderestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 //@EnableSwagger2
--- right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/repository/ArticleRepository.java
+++ right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/repository/ArticleRepository.java
@@ -1,9 +1,31 @@
 package ru.rightcode.rightcoderestservice.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.rightcode.rightcoderestservice.model.Article;
+
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
+    @Override
+    @EntityGraph(attributePaths = {"status", "category", "delete", "editor", "tags", "externalResources", "externalResources.resourceType"})
+    List<Article> findAll();
+
+    @Query("select a from Article a order by a.status.name")
+    List<Article> findAllOrderByStatus();
+
+    @Query("select a from Article a order by a.publicationDate")
+    List<Article> findAllOrderByPublicationDate();
+
+    @Query("select a from Article a order by a.isMainArticle")
+    List<Article> findAllOrderByIsMainArticle();
+
+    @EntityGraph(attributePaths = {"status", "category", "delete", "editor", "tags", "externalResources", "externalResources.resourceType"})
+    @Query(value = "select a from Article a join a.tags t where t.name in (:tags) ")
+    List<Article> findArticlesByTag(@Param("tags") List<String> tags);
 }
