2af99b1c36b80b37439c457f9b8bb7b8f1fb8dec
MikhailBatukhtin <giezz@vk.com>
2023-07-10 22:12:55 +0300

    Устранение опечатки.

--- right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/repository/TagRepository.java
+++ right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/repository/TagRepository.java
@@ -1,6 +1,5 @@
 package ru.rightcode.rightcoderestservice.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
