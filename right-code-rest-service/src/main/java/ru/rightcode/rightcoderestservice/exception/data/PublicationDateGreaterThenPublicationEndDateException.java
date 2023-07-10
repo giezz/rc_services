920fb931a51a6031d4d4b51f6a3832f77da289b9
MikhailBatukhtin <giezz@vk.com>
2023-07-11 00:52:03 +0300

    Сообщение внутри исключения.

--- right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/exception/data/PublicationDateGreaterThenPublicationEndDateException.java
+++ right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/exception/data/PublicationDateGreaterThenPublicationEndDateException.java
@@ -1,7 +1,11 @@
 package ru.rightcode.rightcoderestservice.exception.data;
 
 public class PublicationDateGreaterThenPublicationEndDateException extends RuntimeException {
+
+    private final String message;
+
     public PublicationDateGreaterThenPublicationEndDateException(String message) {
         super(message);
+        this.message = message;
     }
 }
--- right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/exception/data/PublicationEndDateNotSpecifiedException.java
+++ right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/exception/data/PublicationEndDateNotSpecifiedException.java
@@ -2,7 +2,10 @@ package ru.rightcode.rightcoderestservice.exception.data;
 
 public class PublicationEndDateNotSpecifiedException extends RuntimeException {
 
+    private final String message;
+
     public PublicationEndDateNotSpecifiedException(String message) {
         super(message);
+        this.message = message;
     }
 }
