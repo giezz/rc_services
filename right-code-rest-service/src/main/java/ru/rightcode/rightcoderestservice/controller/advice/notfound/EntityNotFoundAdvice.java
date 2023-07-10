649f97d46eaae2fdc6405776ab15344133a9bae0
MikhailBatukhtin <giezz@vk.com>
2023-07-11 00:52:27 +0300

    Отпимизация импортов.

--- right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/controller/advice/notfound/EntityNotFoundAdvice.java
+++ right-code-rest-service/src/main/java/ru/rightcode/rightcoderestservice/controller/advice/notfound/EntityNotFoundAdvice.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.rightcode.rightcoderestservice.dto.ApiErrorResponse;
 import ru.rightcode.rightcoderestservice.exception.BusinessCodeError;
-import ru.rightcode.rightcoderestservice.exception.notfound.ResourceNotFoundException;
 
 @ControllerAdvice
 public class EntityNotFoundAdvice {
